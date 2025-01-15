0ecf9d75831cbf6d4927f6d11359d7ab56ae4316
Kenneth <kennethoaguilar@gmail.com>
2025-01-15 03:23:51 -0800

    s

--- src/extensions/indicators/Lib1.js
+++
@@ -1,3 +0,0 @@
-export default function(id, se, env, lib) {
-
-}
--- src/extensions/indicators/main.js
+++ src/extensions/indicators/main.js
@@ -1,18 +1,15 @@
 import indicators from './indicators.js'
-import Lib1 from './Lib1.js'
 
 // Extension's controller
 export default class Main {
 
     constructor(tv, dc) {
-        this.widgets = {}
         this.tv = tv
         this.dc = dc
 
         this.dc.ww.just('upload-module', {
             id: 'indicator',
-            main: indicators.toString(),  // Mod = main module class (defined as a function)
-            Lib1: Lib1.toString()
+            main: indicators.toString()
         })
     }
 
